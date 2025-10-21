26223ec8bf7142f0d95fa294e3b0a8be21f82f36
Carlos <carlosps.hereman@gmail.com>
2025-10-20 21:58:49 -0300

    corrigir code smells da classe AbstractCrud.java

--- src/main/java/com/cp/data/crud/interfaces/AbstractCrud.java
+++ src/main/java/com/cp/data/crud/interfaces/AbstractCrud.java
@@ -1,48 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.cp.data.crud.interfaces;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 import com.cp.util.AppLog;
-
 import jakarta.persistence.EntityManager;
 
-/**
- *
- * @author utfpr
- */
 public abstract class AbstractCrud<T> {
-    
+
+    private static final Logger logger = Logger.getLogger(AbstractCrud.class.getName());
     private Class<T> entityClass;
 
     protected AbstractCrud(Class<T> entityClass) {
         this.entityClass = entityClass;
     }
 
-
-
     protected abstract EntityManager getEntityManager();
 
+    protected abstract void close();
 
-    protected abstract  void close();
-
-
+    private void executeTransaction(Runnable action) {
+        try {
+            getEntityManager().getTransaction().begin();
+            action.run();
+            getEntityManager().flush();
+            getEntityManager().getTransaction().commit();
+        } catch (Exception e) {
+            getEntityManager().getTransaction().rollback();
+            throw e;
+        }
+    }
 
     public Exception persist(T entity) {
         try {
-            System.out.println("iniciando ########");
-            getEntityManager().getTransaction().begin();
-            getEntityManager().persist(entity);
-            System.out.println("foi o persist $$$$$$");
-            getEntityManager().flush();
-            getEntityManager().getTransaction().commit();
+            logger.info("Iniciando operação de persistência...");
+            executeTransaction(() -> getEntityManager().persist(entity));
+            logger.info("Entidade persistida com sucesso!");
             AppLog.getInstance().info("Registro inserido com sucesso pela classe: " + this.getClass().getName());
             return null;
         } catch (Exception e) {
-            getEntityManager().getTransaction().rollback();
             AppLog.getInstance().warn("Erro ao inserir no banco de dados: " + this.getClass().getName() + "==>" + e.getMessage());
             return e;
         }
@@ -50,24 +46,17 @@ public abstract class AbstractCrud<T> {
 
     public Exception merge(T entity) {
         try {
-            getEntityManager().getTransaction().begin();
-            getEntityManager().merge(entity);
-            getEntityManager().flush();
-            getEntityManager().getTransaction().commit();
+            executeTransaction(() -> getEntityManager().merge(entity));
             AppLog.getInstance().info("Registro alterado com sucesso pela classe: " + this.getClass().getName());
             return null;
         } catch (Exception e) {
-            getEntityManager().getTransaction().rollback();
             return e;
         }
     }
 
     public Exception remove(T entity) {
         try {
-            getEntityManager().getTransaction().begin();
-            getEntityManager().remove(getEntityManager().merge(entity));
-            getEntityManager().flush();
-            getEntityManager().getTransaction().commit();
+            executeTransaction(() -> getEntityManager().remove(getEntityManager().merge(entity)));
             AppLog.getInstance().info("Registro removido com sucesso pela classe: " + this.getClass().getName());
             return null;
         } catch (Exception e) {
@@ -81,32 +70,28 @@ public abstract class AbstractCrud<T> {
 
     public List<T> getAll() {
         try {
-            jakarta.persistence.criteria.CriteriaQuery cq;
-            cq = getEntityManager().getCriteriaBuilder().createQuery();
+            jakarta.persistence.criteria.CriteriaQuery<T> cq = getEntityManager().getCriteriaBuilder().createQuery(entityClass);
             cq.select(cq.from(entityClass));
             return getEntityManager().createQuery(cq).getResultList();
         } catch (Exception e) {
             return Collections.emptyList();
         }
-
     }
 
     public List<T> findRange(int[] range) {
-        jakarta.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        jakarta.persistence.criteria.CriteriaQuery<T> cq = getEntityManager().getCriteriaBuilder().createQuery(entityClass);
         cq.select(cq.from(entityClass));
         jakarta.persistence.Query q = getEntityManager().createQuery(cq);
         q.setMaxResults(range[1] - range[0] + 1);
         q.setFirstResult(range[0]);
-        return Collections.emptyList();
+        return q.getResultList();
     }
 
     public int count() {
-        jakarta.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        jakarta.persistence.criteria.CriteriaQuery<Long> cq = getEntityManager().getCriteriaBuilder().createQuery(Long.class);
         jakarta.persistence.criteria.Root<T> rt = cq.from(entityClass);
         cq.select(getEntityManager().getCriteriaBuilder().count(rt));
         jakarta.persistence.Query q = getEntityManager().createQuery(cq);
         return ((Long) q.getSingleResult()).intValue();
     }
-
-
 }
